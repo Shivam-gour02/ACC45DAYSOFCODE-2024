c34ee44b7baaa17508fb406c9dcf42dba90244f7
Shivam-gour02 <shivamgour2005@gmail.com>
2024-09-29 06:05:36 +0530

    Add files via upload

---
+++ Day1-CWC23QUALIF.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+	int s;
+	scanf("%d",&s);
+	//check if the team has qualified
+	if(s>=12){
+	    printf("Yes");
+	    
+	    }else{
+	        printf("No");
+	    }
+return 0;
+}
+
